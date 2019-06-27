72a2dbe074dfa9abe3b3d969be770ac0a52079a5
Tina-Meryl AMANS <tamans@arwen.ibcp.fr>
2019-06-27 15:46:41 +0200

    dt20

---
+++ test_angle/ac5_1azs_c1c2_golf/ang_h.sh
@@ -0,0 +1,8 @@
+/mobi/group/AC5_Trajectories_code/f90/helix <<!
+ &THE_INPUT traj='ac5_1azs_c1c2_golfpbock_dt20_centerpbc',out='tt_a',tstart=400000,nhel=2 &end
+20
+36  45
+93  100
+224 232
+286 296
+!
